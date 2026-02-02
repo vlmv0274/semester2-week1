6b694a4123f8decf99d63ca56e96bfa7b9cf0ca1
vlmv0274 <vlmv0274@leeds.ac.uk>
2026-02-02 16:27:54 +0000

    Update question1.sql

--- worksheet/task2/question1.sql
+++ worksheet/task2/question1.sql
@@ -1,3 +1,8 @@
 -- List all students who enrolled in the year 2022.
 -- Expected Columns:
 -- StudentId, FirstName, LastName, EnrolmentYear
+SELECT StudentID, FirstName, LastName, EnrolmentYear
+
+FROM
+
+student WHERE EnrolmentYear = 2022;
