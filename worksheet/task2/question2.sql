cbb5b7f1a9bcb1cce4189b2c5b38765e92920bac
vlmv0274 <vlmv0274@leeds.ac.uk>
2026-02-02 16:29:25 +0000

    Update question2.sql

--- worksheet/task2/question2.sql
+++ worksheet/task2/question2.sql
@@ -1,3 +1,8 @@
 -- Find the 5 youngest students in the database.
 -- Expected Columns:
 -- StudentId, FirstName, LastName, DateOfBirth
+SELECT StudentID, FirstName, LastName, DateOfBirth
+
+FROM
+
+student ORDER BY DateOfBirth ASC LIMIT 5;
