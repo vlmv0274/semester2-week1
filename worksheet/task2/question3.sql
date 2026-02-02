5315ac4e49afaaf1550e12b886cdc6f24c748c2b
vlmv0274 <vlmv0274@leeds.ac.uk>
2026-02-02 16:35:50 +0000

    Update question3.sql

--- worksheet/task2/question3.sql
+++ worksheet/task2/question3.sql
@@ -1,3 +1,8 @@
 -- For each department, calculate the number of enrolments in its courses.
 -- Expected Columns:
 -- DepartmentName, TotalEnrolments
+--SELECT DepartmentName, TotalEnrolments
+
+--FROM
+
+--student ORDER BY DateOfBirth ASC LIMIT 5;
