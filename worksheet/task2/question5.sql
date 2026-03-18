ad46d18e662b469a7414084af7c9f77686ecff2f
vlmv0274 <vlmv0274@leeds.ac.uk>
2026-03-18 05:47:05 +0000

    Update question5.sql

--- worksheet/task2/question5.sql
+++ worksheet/task2/question5.sql
@@ -1,6 +1,6 @@
 SELECT Student.StudentId, Student.FirstName, Student.LastName, COALESCE(SUM(Course.Credits), 0) AS TotalCreditsPassed
 FROM Student
-LEFT JOIN Enrolment ON Student.StudentId = Enrolment.StudentId
-LEFT JOIN Course ON Enrolment.CourseId = Course.CourseId AND Enrolment.Grade >= 40
+LEFT JOIN Enrolment ON Student.StudentId = Enrolment.StudentId AND Enrolment.Grade >= 40
+LEFT JOIN Course ON Enrolment.CourseId = Course.CourseId
 GROUP BY Student.StudentId, Student.FirstName, Student.LastName
 ORDER BY Student.StudentId;
