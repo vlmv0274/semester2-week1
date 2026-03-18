cc183667289e9b5f7d7e580fe8bd8c7cdd6499be
vlmv0274 <vlmv0274@leeds.ac.uk>
2026-03-18 05:32:02 +0000

    Update question4.sql

--- worksheet/task2/question4.sql
+++ worksheet/task2/question4.sql
@@ -1,8 +1,13 @@
 -- List all students along with the names of the courses they are enrolled in.
+
 -- Expected Columns:
+
 -- StudentId, FirstName, LastName, CourseName
-SELECT StudentID, FirstName, LastName, CourseName
 
-FROM
+SELECT Student.StudentID, Student.FirstName, Student.LastName, Course.CourseName
 
-student ORDER BY StudentID;
+FROM student
+
+JOIN Enrolment ON Enrolment.StudentID = Student.StudentID
+
+JOIN Course ON Course.CourseID = Enrolment.CourseID;
