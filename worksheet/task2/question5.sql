4d185e51b03ea81633be7ab6fbbfd73a3fab7d78
vlmv0274 <vlmv0274@leeds.ac.uk>
2026-03-22 20:59:12 +0000

    Update question5.sql

--- worksheet/task2/question5.sql
+++ worksheet/task2/question5.sql
@@ -1,6 +1,6 @@
-SELECT Student.StudentId, Student.FirstName, Student.LastName, COALESCE(SUM(Course.Credits), 0) AS TotalCreditsPassed
+SELECT Student.StudentId, Student.FirstName, Student.LastName, COALESCE(SUM(CASE WHEN Enrolment.Grade >= 40 THEN Course.Credits ELSE 0 END), 0) AS TotalCreditsPassed
 FROM Student
-LEFT JOIN Enrolment ON Student.StudentId = Enrolment.StudentId AND Enrolment.Grade >= 40
+LEFT JOIN Enrolment ON Student.StudentId = Enrolment.StudentId
 LEFT JOIN Course ON Enrolment.CourseId = Course.CourseId
 GROUP BY Student.StudentId, Student.FirstName, Student.LastName
 ORDER BY Student.StudentId;
